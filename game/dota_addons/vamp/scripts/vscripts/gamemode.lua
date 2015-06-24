515c2ae9badc4d93be01143de5d344c4fdf5b554
Fappingsock <Magnusharestadjunk@gmail.com>
2015-06-24 19:20:23 +0200

    fixed

--- game/dota_addons/vamp/scripts/vscripts/gamemode.lua
+++ game/dota_addons/vamp/scripts/vscripts/gamemode.lua
@@ -8,12 +8,6 @@ PRE_GAME_TIME = 0
 HERO_SELECT_TIME = 10
 
 function _VampMode:InitGameMode()
-
-
-
-
-
-	
 	GameRules:SetHeroSelectionTime(HERO_SELECT_TIME)
 	GameRules:SetPreGameTime(PRE_GAME_TIME)
 	GameRules:SetCustomGameSetupRemainingTime(0)
